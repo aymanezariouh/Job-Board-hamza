6fb70c31fb61fc12b98f283ec897f899ec44af54
aymanezariouh <aymanezariouh@gmail.com>
2026-09-15 16:44:54 +0100

    fix:imported the js file in app

--- js/app.js
+++ js/app.js
@@ -1,13 +1,21 @@
-const container = document.querySelector("#offers-container");
+import { fetchOffers } from "./data.js";
+import { renderOffers, renderLoading, renderError } from "./render.js";
+import { filterState, applyFilters } from "./filters.js";
+
+// Select your HTML container element
+const container = document.querySelector("#offers-container"); // Adjust selector to match your HTML
+
+let allOffers = [];
 
 async function init() {
-  try{
+  try {
     renderLoading(container);
-  const offers = await fetchOffers();
-
-  renderOffers(offers, container);
-  }catch(error){
+    allOffers = await fetchOffers();
+    renderOffers(allOffers, container);
+  } catch (error) {
+    console.error(error);
     renderError(container);
   }
 }
+
 init();
